a1407335f11da85fc50aca81f14055d8a93fa7fa
brunetpantoja <vickymanobanmanoban@gmail.com>
2025-09-25 09:41:23 -0300

    Update RepetiçaoFor.php

--- RepetiçaoFor.php
+++ RepetiçaoFor.php
@@ -1,7 +1,7 @@
 <?php
-if(!empty($_GET['numero'])&& is_numeric($_GET['numero'])){
+if(!empty($_POST['numero'])&& is_numeric($_GET['numero'])){
     $numero = $_POST['numero'];
-for($cont = 0; $cont <= 10 ; $cont++){
+for($cont = 0; $cont <= 10 ;$cont++){
     echo nl2br("$numero + $cont =".$numero + $cont. "\n");
 }
 }else{
@@ -12,7 +12,6 @@ for($cont = 0; $cont <= 10 ; $cont++){
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
@@ -20,6 +19,8 @@ for($cont = 0; $cont <= 10 ; $cont++){
     <form method= "POST">
         <label>Informe um número: </label>
         <input type= "number" name="numero" placeholder="Digite um número" />
+        <input type="submit" value="Enviar" />
  </form>
 </body>
+
 </html>
